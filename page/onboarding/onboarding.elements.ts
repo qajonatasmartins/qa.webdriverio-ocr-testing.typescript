5e48814feb323d7517c8d4b18dc0f7360f75fb9c
Jonatas Faria <jonatas.martins@zig.fun>
2024-11-24 18:50:10 -0300

    .

--- page/onboarding/onboarding.elements.ts
+++ page/onboarding/onboarding.elements.ts
@@ -3,12 +3,12 @@ export default class OnboardingElements {
     /**
      * Mapeamento do botão [Pular OnBoarding]
      */
-    get btnSkipOnBoarding() { return $("=Pular OnBoarding") }
+    get btnSkipOnBoarding() { return $("//flt-semantics[contains(text(),'Pular OnBoarding')]") }
 
     /**
      * Mapeamento do botão de [Criar uma super conta]
      */
-    get btnCreateNewSuperAccount() { return $("=Criar Uma Super Conta") }
+    get btnCreateNewSuperAccount() { return $("//flt-semantics[contains(text(),'Criar Uma Super Conta')]") }
 
     get flutterWeb() { return $("flt-semantics-placeholder[role='button']") }
 
