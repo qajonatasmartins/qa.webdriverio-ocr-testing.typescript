8004050d6c94bf92d8b2926818f2682149fdceda
Jonatas Faria <jonatas.martins@zig.fun>
2024-11-24 18:10:39 -0300

    .

--- data/login/login.data.ts
+++ data/login/login.data.ts
@@ -1,6 +1,5 @@
 export const ct0001 = {
     email: `${process.env.EMAIL}`,
     password: `${process.env.PASSWORD}`,
-    // titlePage: 'Balanceador de Times',
     userLogged: 'Conectado a\nRodrigo\nrodrigo.camini@gmail.com'
 }
